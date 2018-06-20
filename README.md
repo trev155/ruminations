d18683914f1408c2edbf62d9c74c6807ab4e2f22
Trevor <trevku155@gmail.com>
2018-06-20 13:18:36 -0400

    Bar graph for class names for ecommerce clothing reviews

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 Analysis of datasets
 
 Datasets used (temp):
-https://www.kaggle.com/unitednations/gender-info-2007  
-https://www.kaggle.com/jonavery/incomes-by-career-and-gender  
-https://www.kaggle.com/osmi/mental-health-in-tech-survey  
-https://www.kaggle.com/monkey09/women-e-clothing-reviews  
+https://www.kaggle.com/unitednations/gender-info-2007
+https://www.kaggle.com/osmi/mental-health-in-tech-survey
 
 ## About
 A repository where I do data analysis stuff on various datasets. 
@@ -14,5 +12,15 @@ All results will be displayed here for now.
 
 ## Results
 ### U.S. Incomes by Occupation and Gender
-to come...
+Uses the following dataset:
+https://www.kaggle.com/jonavery/incomes-by-career-and-gender
 
+Here is a plot showing weekly median salaries for both men and women across all industries:
+<img src="incomes_career_gender/salaries.png" width="800" height="400">
+
+### Women's Ecommerce Clothing Reviews
+Uses this dataset:
+https://www.kaggle.com/monkey09/women-e-clothing-reviews
+
+What are the most commonly reviewed item classes from the dataset?
+<img src="women_ecommerce/item_types.png" width="800" height="400">
--- women_ecommerce/ecommerce.py
+++ women_ecommerce/ecommerce.py
@@ -1,63 +1,47 @@
-# About the Dataset
+# -- About the Dataset
+"""
+https://www.kaggle.com/monkey09/women-e-clothing-reviews
+Ecommerce reviews of women's clothing items.
+
+Column Descriptions of relevant columns (note that there are several NaNs throughout):
+Age - Reviewer’s age.
+Title - Review Title.
+Review - Text Review body.
+Rating - Rating of the product from 1 Worst, to 5 Best.
+Recommended IND - whether Customer recommends the product where 1 is recommended, 0 is not recommended.
+Positive Feedback Count - Whether other customers found the review helpful.
+Division Name - Categorical name of the product high level division.
+Department Name - Categorical name of the product department name.
+Class Name - Categorical name of the product class name.
 """
 
-"""
-
-# Notes
-"""
-INCOMPLETE
-"""
-
-# Import libraries
+# -- Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import collections
+import wordcloud
 
-# Import dataset
-## column indexes 2, 5 are: Age, Rating
-## column index 6 is: recommended
-dataset = pd.read_csv("data/womens_clothing_ecommerce_reviews.csv")
-X = dataset.iloc[:, [2, 5]].values
-y = dataset.iloc[:, 6].values
+# -- Import dataset
+dataset = pd.read_csv("womens_clothing_ecommerce_reviews.csv")
+# retain only the relevant columns
+dataset = dataset.iloc[:, 2:]
+dataset = dataset.drop(["Division Name", "Department Name"], axis=1)
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# -- Simple plot of number of reviews per class name
+# get classes and counts
+classes = [c for c in dataset.iloc[:, -1].values]
+unique, counts = np.unique(classes, return_counts=True)
+class_counts = dict(zip(unique, counts))
+del class_counts["nan"]
+class_counts = collections.OrderedDict(sorted(class_counts.items(), key=lambda x: x[1], reverse=True))
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Visualising the results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Rating')
-plt.legend()
-plt.show()
-
+# create bar graph
+num_classes = len(class_counts)
+fig, ax = plt.subplots()
+plt.bar(np.arange(num_classes), class_counts.values(), color="Navy")
+plt.xticks(np.arange(num_classes), class_counts.keys(), fontsize=18, rotation=80)
+ax.tick_params(axis="y", which="major", labelsize=20)
+plt.title("Number of Reviews for each Item Type", fontsize=32)
+plt.ylabel("Number of Reviews", fontsize=24)
 
