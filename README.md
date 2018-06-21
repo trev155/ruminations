3b46e21d25ba2015eeb485a0a0e574cce6895f01
Trevor <trevku155@gmail.com>
2018-06-20 22:49:30 -0400

    Bag of words model for Women's eCommerce clothing reviews. Refactoring.

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ A repository where I do data analysis stuff on various datasets.
 Each subdirectory will contain a different dataset / script that processes that dataset. 
 All results will be displayed here for now. 
 
-## Results
-### U.S. Incomes by Occupation and Gender
+# Results
+## U.S. Incomes by Occupation and Gender
 Uses the following dataset:
 https://www.kaggle.com/jonavery/incomes-by-career-and-gender
 
@@ -22,16 +22,37 @@ Well for sure, men earn more than women in almost all industries.
 Men outnumber women in certain industries such as Engineering, Construction, and Maintenance.
 Women outnumber men in industries like Office, Healthcare, and Education.
 
-Now, as the dataset does not contain any other contextual data (eg. years of experience, position, etc.), it is unfair to conclude any more than what this above data shows.
+Now, as the dataset does not contain any other contextual data (eg. years of experience, position, etc.), it is difficult to conclude any more than what this above data shows.
 
 
-### Women's Ecommerce Clothing Reviews
+## Women's Ecommerce Clothing Reviews
 Uses this dataset:
 https://www.kaggle.com/monkey09/women-e-clothing-reviews
 
 What are the most commonly reviewed item classes from the dataset?
 <img src="women_ecommerce/item_types.png" width="800" height="400">
 
-### Mental Health in Tech
+Perhaps unsurprisingly, `Dresses` is the most reviewed item.
+
+Next, I try my hand at some NLP. I'm following my notes from the Udemy course I'm doing.
+First, I take the review texts of the first 1000 reviews (I don't take all of them, since it takes really long
+for this method to process all the comments), and I clean them up - stemming, remove punctuation, etc.
+Then, I fit a Bag-of-words model, and then fit a classifier to the resulting
+document-term matrix. Now I have an array of 1000 cleaned strings, and then we split this array
+into a test and training set. A confusion matrix is generated for the test set performance.
+
+<img src="women_ecommerce/confusion_1000.png" width="400" height="300">
+
+Our accuracy is pretty good, but there is a problem.
+Although we successfully predict cases where the product is recommended,
+we don't predict cases where the product is not recommended.
+
+Let's try again, but with 5000 reviews.
+
+<img src="women_ecommerce/confusion_5000.png" width="400" height="300">
+
+Oof. that's not any better. Perhaps review text is not a good predictor for whether the reviewer recommend the product.
+
+## Mental Health in Tech
 Dataset: https://www.kaggle.com/osmi/mental-health-in-tech-survey
 
--- women_ecommerce/ecommerce.py
+++ women_ecommerce/ecommerce.py
@@ -15,32 +15,109 @@ Department Name - Categorical name of the product department name.
 Class Name - Categorical name of the product class name.
 """
 
-# -- Import libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import collections
-import wordcloud
 
-# -- Import dataset
-dataset = pd.read_csv("womens_clothing_ecommerce_reviews.csv")
-# retain only the relevant columns
-dataset = dataset.iloc[:, 2:]
-dataset = dataset.drop(["Division Name", "Department Name"], axis=1)
+def bar_graph_class_names():
+    """
+    Simple plot of number of reviews per class name
+    """
+    # get classes and counts
+    classes = [c for c in df.iloc[:, -1].values]
+    unique, counts = np.unique(classes, return_counts=True)
+    class_counts = dict(zip(unique, counts))
+    del class_counts["nan"]
+    class_counts = collections.OrderedDict(sorted(class_counts.items(), key=lambda x: x[1], reverse=True))
 
-# -- Simple plot of number of reviews per class name
-# get classes and counts
-classes = [c for c in dataset.iloc[:, -1].values]
-unique, counts = np.unique(classes, return_counts=True)
-class_counts = dict(zip(unique, counts))
-del class_counts["nan"]
-class_counts = collections.OrderedDict(sorted(class_counts.items(), key=lambda x: x[1], reverse=True))
+    # create bar graph
+    num_classes = len(class_counts)
+    fig, ax = plt.subplots()
+    plt.bar(np.arange(num_classes), class_counts.values(), color="Navy")
+    plt.xticks(np.arange(num_classes), class_counts.keys(), fontsize=18, rotation=80)
+    ax.tick_params(axis="y", which="major", labelsize=20)
+    plt.title("Number of Reviews for each Item Type", fontsize=32)
+    plt.ylabel("Number of Reviews", fontsize=24)
 
-# create bar graph
-num_classes = len(class_counts)
-fig, ax = plt.subplots()
-plt.bar(np.arange(num_classes), class_counts.values(), color="Navy")
-plt.xticks(np.arange(num_classes), class_counts.keys(), fontsize=18, rotation=80)
-ax.tick_params(axis="y", which="major", labelsize=20)
-plt.title("Number of Reviews for each Item Type", fontsize=32)
-plt.ylabel("Number of Reviews", fontsize=24)
+
+def bag_of_words():
+    """
+    Building a bag-of-words-model for the review texts.
+    This function takes a long time to execute due to the size of the data set and the time it takes to process texts.
+    """
+    # -- Cleaning the texts
+    import re
+    # import nltk
+    # nltk.download('stopwords')
+    from nltk.corpus import stopwords
+    from nltk.stem.porter import PorterStemmer
+    corpus = []
+    corpus_size = 5000    # if we were to iterate over all of it, it would take way too long
+    for i in range(corpus_size):
+        review_text = df["Review Text"][i]
+        if type(review_text) != str:
+            corpus.append("")
+            continue
+        review = re.sub('[^a-zA-Z]', " ", review_text)
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review = ' '.join(review)
+        corpus.append(review)
+
+    # Creating the Bag of Words model
+    from sklearn.feature_extraction.text import CountVectorizer
+    cv = CountVectorizer(max_features=1500)
+    X = cv.fit_transform(corpus).toarray()
+    y = df.iloc[:corpus_size, 4].values
+
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+    # Fitting Naive Bayes to the Training set
+    from sklearn.naive_bayes import GaussianNB
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Plot the confusion matrix
+    classNames = ["no", "yes"]
+    ticks = np.arange(len(classNames))
+    labels = [["TN", "FP"], ["FN", "TP"]]
+
+    plt.clf()
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Wistia)
+    plt.title("Product Recommendations", fontsize=36)
+    plt.ylabel("True Label", fontsize=32)
+    plt.xlabel("Predicted Label", fontsize=32)
+    plt.xticks(ticks, classNames, rotation=45, fontsize=24)
+    plt.yticks(ticks, classNames, fontsize=24)
+    for i in range(len(classNames)):
+        for j in range(len(classNames)):
+            plt.text(j, i, str(labels[i][j]) + " = " + str(cm[i][j]), fontsize=32)
+    plt.show()
+
+
+if __name__ == "__main__":
+    # -- Import libraries
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import collections
+
+    # -- Import dataset
+    df = pd.read_csv("womens_clothing_ecommerce_reviews.csv")
+    # retain only the relevant columns
+    df = df.iloc[:, 2:]
+    df = df.drop(["Division Name", "Department Name"], axis=1)
+
+    # -- Create bar graph of class names
+    bar_graph_class_names()
+
+    # -- Bag of words model and classification for review texts
+    bag_of_words()
